d780b872512a152db82b008fe086f0c3333ce6a5
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-02 15:29:01 -0400

    removed the create arguement from 6-route53_certificate.tf file

--- infrastructure/6-route53_certificate.tf
+++ infrastructure/6-route53_certificate.tf
@@ -20,7 +20,6 @@ resource "aws_route53_record" "cert_validation_record" {
   name            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_name
   records         = [tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_value]
   type            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_type
-  create          = length(aws_route53_record.cert_validation_record_lookup[count.index].id) == 0 
 }
 
 // Lookup the existing record to check if it already exists
@@ -29,9 +28,6 @@ resource "aws_route53_record" "cert_validation_record_lookup" {
   zone_id = data.aws_route53_zone.main.zone_id[count.index]
   name    = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_name
   type    = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_type
-
-  // Only lookup the record if it exists already
-  create = length(aws_route53_record.cert_validation_record_lookup[count.index].id) > 0 
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
