6f0338ee72d1379619a300e68b0e896848abc611
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-02 15:39:36 -0400

    allow overwrite changed to true - 6-route53_certificate.tf ln 16, errored.tfstate ln 1572

--- infrastructure/6-route53_certificate.tf
+++ infrastructure/6-route53_certificate.tf
@@ -13,24 +13,15 @@ resource "aws_acm_certificate" "dev_cert" {
 }
 
 resource "aws_route53_record" "cert_validation_record" {
-  count           = length(aws_acm_certificate.dev_cert.domain_validation_options)
-  allow_overwrite = false
+  allow_overwrite = true
   ttl             = 60
-  zone_id         = data.aws_route53_zone.main.zone_id[count.index]
-  name            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_name
-  records         = [tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_value]
-  type            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_type
-}
-
-// Lookup the existing record to check if it already exists
-resource "aws_route53_record" "cert_validation_record_lookup" {
-  count   = length(aws_acm_certificate.dev_cert.domain_validation_options)
-  zone_id = data.aws_route53_zone.main.zone_id[count.index]
-  name    = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_name
-  type    = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[count.index].resource_record_type
+  zone_id         = data.aws_route53_zone.main.zone_id
+  name            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_type
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.dev_cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation_record.*.fqdn
+  validation_record_fqdns = [aws_route53_record.cert_validation_record.fqdn]
 }
