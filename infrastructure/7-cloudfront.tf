0bac37f47bd38543f692b3b12a7fc7491fba40fc
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-02 17:11:03 -0400

    orig #7 file

--- infrastructure/7-cloudfront.tf
+++ infrastructure/7-cloudfront.tf
@@ -1,13 +1,8 @@
-
 resource "aws_cloudfront_origin_access_identity" "oai" {
   comment = "OAI for ${var.dev_client_app_domain}"
 }
 
-resource "aws_cloudfront_distribution" "talktechapp_cloudfront_distribution" {
-  # id is not a supported argument. Remove this line.
-  # id = "existing_distribution_id"
-  
-  # These arguments are all supported.
+resource "aws_cloudfront_distribution" "chatapp_cloudfront_distribution" {
   retain_on_delete    = false
   price_class         = "PriceClass_All"
   enabled             = true
@@ -16,18 +11,17 @@ resource "aws_cloudfront_distribution" "talktechapp_cloudfront_distribution" {
   aliases             = [var.dev_client_app_domain]
 
   origin {
-    domain_name = aws_s3_bucket.talktechapp_s3_bucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.talktechapp_s3_bucket.id
+    domain_name = aws_s3_bucket.chatapp_s3_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.chatapp_s3_bucket.id
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
 
   default_cache_behavior {
-    # These arguments are all supported.
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id       = aws_s3_bucket.talktechapp_s3_bucket.id
+    target_origin_id       = aws_s3_bucket.chatapp_s3_bucket.id
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
     min_ttl                = 0
@@ -35,37 +29,34 @@ resource "aws_cloudfront_distribution" "talktechapp_cloudfront_distribution" {
     max_ttl                = 300
 
     forwarded_values {
-      # This argument is supported.
       query_string = true
       cookies {
         forward = "all"
       }
     }
   }
-  
-  # This block is supported. Replace var.custom_error_response with a map of error responses.
-  custom_error_response {
+
+  dynamic "custom_error_response" {
     for_each = var.custom_error_response
-    error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
-    error_code            = custom_error_response.value.error_code
-    response_code         = custom_error_response.value.response_code
-    response_page_path    = custom_error_response.value.response_page_path
+    content {
+      error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
+      error_code            = custom_error_response.value.error_code
+      response_code         = custom_error_response.value.response_code
+      response_page_path    = custom_error_response.value.response_page_path
+    }
   }
 
   restrictions {
-    # This block is supported.
     geo_restriction {
       restriction_type = "none"
     }
   }
 
   viewer_certificate {
-    # These arguments are all supported.
     acm_certificate_arn      = aws_acm_certificate.dev_cert.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
 
-  # This argument is supported. Make sure local.common_tags is defined elsewhere in your Terraform code.
   tags = local.common_tags
 }
